f1bbe2242e7d893b5f06664a400ee5cc77ab2a8f
Kelly Savietta <ksavietta@gmail.com>
2018-04-07 17:35:51 -0500

    feat(image) [KS] add an image for the landing page

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent implements OnInit {
+  public defaultPic: string = '../../assets/images/home/image1.jpg';
 
   constructor() { }
 
