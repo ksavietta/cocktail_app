6051ab1c2553f537329a12c10ddc08cd40ffd786
Kelly Savietta <ksavietta@gmail.com>
2018-04-07 12:02:06 -0500

    feat(init) [KS] enable cors and add example components

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,18 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-
 import { AppComponent } from './app.component';
-
+import { ApiService } from './api.service';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductAddComponent,
+    ProductListComponent,
+    OrderAddComponent,
+    CustomerAddComponent,
+    OrderListComponent,
+    CustomerListComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [ApiService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
