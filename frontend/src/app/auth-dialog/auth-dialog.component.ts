6958b280e9fb5964d9963a265113a6d025a845c0
Kelly Savietta <ksavietta@gmail.com>
2018-06-10 17:44:54 -0500

    feat(auth) [KS] Get Modals working, work on login modal

--- frontend/src/app/auth-dialog/auth-dialog.component.ts
+++ frontend/src/app/auth-dialog/auth-dialog.component.ts
@@ -1,13 +1,18 @@
 import { Component, OnInit } from '@angular/core';
+import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
 
 @Component({
   selector: 'app-auth-dialog',
   templateUrl: './auth-dialog.component.html',
-  styleUrls: ['./auth-dialog.component.css']
+  styleUrls: ['./auth-dialog.component.sass']
 })
 export class AuthDialogComponent implements OnInit {
+  // @Input('auth-mode') authMode: 'login' | 'register' = 'login';
 
-  constructor() { }
+  constructor(public bsModalRef: BsModalRef) { }
+
+  // isLoginMode(){return this.authMode == 'login'}
+  // isRegisterMode(){return this.authMode == 'register'}
 
   ngOnInit() {
   }
