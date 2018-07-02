be6e18bb9b2de405314f4b4a43fae3ce918a361d
Kelly Savietta <ksavietta@gmail.com>
2018-07-02 16:48:54 -0400

    feat(auth) [KS] Add dialog content component

--- frontend/src/app/auth-dialog/auth-dialog.component.ts
+++ frontend/src/app/auth-dialog/auth-dialog.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { BsModalService } from 'ngx-bootstrap/modal';
 import { BsModalRef } from 'ngx-bootstrap/modal/bs-modal-ref.service';
+import { AuthDialogContentComponent } from '../auth-dialog-content/auth-dialog-content.component'
+
 
 @Component({
   selector: 'app-auth-dialog',
@@ -15,19 +17,11 @@ export class AuthDialogComponent implements OnInit {
   constructor(private modalService: BsModalService) { }
   ngOnInit() {}
 
-  // openDialog(mode: 'login' | 'register' = 'login') {
-  //   this.authMode = mode;
-  //   this.modalRef = this.modalService.show(this);
-  // }
-
-  closeDialog() {
-    this.modalRef.hide()
-  }
-
   isLoginMode(){return this.authMode == 'login'}
   isRegisterMode(){return this.authMode == 'register'}
 
-  openDialog() {
+  openDialog(mode: 'login' | 'register' = 'login') {
+    this.authMode = mode;
     const initialState = {
       list: [
         'Open a modal with component',
@@ -37,43 +31,11 @@ export class AuthDialogComponent implements OnInit {
       ],
       title: 'Modal with component'
     };
-    this.modalRef = this.modalService.show(ModalContentComponent, {initialState});
+    this.modalRef = this.modalService.show(AuthDialogContentComponent, {initialState});
     this.modalRef.content.closeBtnName = 'Close';
   }
-}
 
-}
-
-/* This is a component which we pass in modal*/
-
-@Component({
-  selector: 'modal-content',
-  template: `
-    <div class="modal-header">
-      <h4 class="modal-title pull-left">{{title}}</h4>
-      <button type="button" class="close pull-right" aria-label="Close" (click)="bsModalRef.hide()">
-        <span aria-hidden="true">&times;</span>
-      </button>
-    </div>
-    <div class="modal-body">
-      <ul *ngIf="list.length">
-        <li *ngFor="let item of list">{{item}}</li>
-      </ul>
-    </div>
-    <div class="modal-footer">
-      <button type="button" class="btn btn-default" (click)="bsModalRef.hide()">{{closeBtnName}}</button>
-    </div>
-  `
-})
-
-export class ModalContentComponent implements OnInit {
-  title: string;
-  closeBtnName: string;
-  list: any[] = [];
-
-  constructor(public bsModalRef: BsModalRef) {}
-
-  ngOnInit() {
-    this.list.push('PROFIT!!!');
+  closeDialog() {
+    this.modalRef.hide()
   }
 }
