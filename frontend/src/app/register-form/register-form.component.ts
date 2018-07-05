685b973e1ecb2560c31e731449ee26fb8a155a46
Kelly Savietta <ksavietta@gmail.com>
2018-07-05 16:52:44 -0400

    feat(auth) [KS] add auth service

--- frontend/src/app/register-form/register-form.component.ts
+++ frontend/src/app/register-form/register-form.component.ts
@@ -1,5 +1,5 @@
 import {Component, OnInit, EventEmitter, Output} from '@angular/core';
-import {Angular2TokenService} from "angular2-token";
+import {AuthService} from "../services/auth.service";
 
 @Component({
   selector: 'app-register-form',
@@ -16,21 +16,17 @@ export class RegisterFormComponent implements OnInit {
 
   @Output() onFormResult = new EventEmitter<any>();
 
-  constructor(private tokenAuthSerivce:Angular2TokenService) { }
+  constructor(private authService:AuthService) { }
 
   ngOnInit() {}
 
   onSignUpSubmit(){
-    this.tokenAuthSerivce.registerAccount(this.signUpUser).subscribe(
-
+    this.authService.registerUser(this.signUpUser).subscribe(
         (res) => {
-
           if (res.status == 200){
             this.onFormResult.emit({signedUp: true, res})
           }
-
         },
-
         (err) => {
           console.log(err.json())
           this.onFormResult.emit({signedUp: false, err})
